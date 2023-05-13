1242693116a091b08253811d615b93079f574eea
Aarun Jury <aarunjury@me.com>
2023-05-13 16:50:30 +1000

    Bug fixes

--- 10week/task2_3/travel/models.py
+++ 10week/task2_3/travel/models.py
@@ -1,7 +1,8 @@
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users' # good practice to specify table name
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), index = True, unique = True, nullable=False)

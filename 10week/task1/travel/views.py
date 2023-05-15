26cbf626041bd39ba10503cc565fc8b348fa275a
Aarun Jury <aarunjury@me.com>
2023-05-15 15:24:15 +1000

    Linting

--- 10week/task1/travel/views.py
+++ 10week/task1/travel/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect,url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Destination
 from . import db
 

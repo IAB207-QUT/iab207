8df26e6c9371b6c315891d51204999d24defda84
Aarun Jury <aarunjury@me.com>
2023-05-15 15:25:28 +1000

    Less lint

--- 10week/task1/travel/views.py
+++ 10week/task1/travel/views.py
@@ -13,7 +13,7 @@ def index():
 def search():
     if request.args['search'] and request.args['search'] != "":
         print(request.args['search'])
-        query = "%" + request.args['search'] + '%'
+        query = "%" + request.args['search'] + "%"
         destinations = db.session.scalars(db.select(Destination)).where(Destination.description.like(query))
         return render_template('index.html', destinations = destinations)
     else:
